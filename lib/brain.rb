257ec393a2e40db389cee731cd1d6a38408b5cfc
brainopia <ravwar@gmail.com>
2008-06-28 23:58:46 +0400

    Include extensions in layout

--- lib/brain.rb
+++ lib/brain.rb
@@ -2,5 +2,9 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Brain
-  
 end
+
+module Brain::Extensions
+end
+
+Brain.send :include, Brain::Extensions
